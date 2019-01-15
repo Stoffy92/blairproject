4d91b7e2facaa26c362af676feb6f46369706384
Stefan <stoffberg.stefan@gmail.com>
2019-01-16 09:55:18 +1300

    optmized hero img for performance

--- public/javascript.js
+++ public/javascript.js
@@ -40,3 +40,28 @@ function initializeClock(id, endtime) {
 var deadline = new Date(Date.parse(new Date('07/20/2019 10:1 AM')));
 initializeClock('clockdiv', deadline);
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
