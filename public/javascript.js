84facb0073c8e2923f92ee4fa9304e4903bef630
Stefan <38707041+Stoffy92@users.noreply.github.com>
2019-07-10 19:38:23 +1200

    Update javascript.js
    Changed reception address, removed zombie code

--- public/javascript.js
+++ public/javascript.js
@@ -81,12 +81,12 @@ function receptionAddress() {
 	)
 		//  window.open("maps://maps.google.com/maps?daddr=Taling Ngam, Ko Samui District, Surat Thani 84140, Thailand");
 		window.open(
-			'https://www.google.co.nz/maps/place/Villa+Laem+Sor/@9.4110593,99.9598396,16.33z/data=!4m12!1m6!3m5!1s0x3054f4cece7cbc0d:0xec45e773bcff5e9a!2sVilla+Laem+Sor!8m2!3d9.4111329!4d99.9628853!3m4!1s0x3054f4cece7cbc0d:0xec45e773bcff5e9a!8m2!3d9.4111329!4d99.9628853'
+			'https://www.google.com.au/maps/place/Miskawaan+Luxury+Beachfront+Villas/@9.56952,99.9985453,17z/data=!3m1!4b1!4m8!3m7!1s0x3054f0afa375b3b3:0xa414b9d7b6f37bec!5m2!4m1!1i2!8m2!3d9.56952!4d100.000734'
 		);
 	//  window.open("maps://maps.google.com/maps?daddr=Taling Ngam, Ko Samui District, Surat Thani 84140, Thailand");
 	else
 		window.open(
-			'https://www.google.co.nz/maps/place/Villa+Laem+Sor/@9.4110593,99.9598396,16.33z/data=!4m12!1m6!3m5!1s0x3054f4cece7cbc0d:0xec45e773bcff5e9a!2sVilla+Laem+Sor!8m2!3d9.4111329!4d99.9628853!3m4!1s0x3054f4cece7cbc0d:0xec45e773bcff5e9a!8m2!3d9.4111329!4d99.9628853'
+			'https://www.google.com.au/maps/place/Miskawaan+Luxury+Beachfront+Villas/@9.56952,99.9985453,17z/data=!3m1!4b1!4m8!3m7!1s0x3054f0afa375b3b3:0xa414b9d7b6f37bec!5m2!4m1!1i2!8m2!3d9.56952!4d100.000734'
 		);
 }
 
@@ -139,92 +139,4 @@ document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener("orientationChange", lazyload);
 });
 
-// $(document).ready(function() {
-//   $(".submit").click(function() {
-//     $(".submit").addClass("loading");
-//     setTimeout(function() {
-//       $(".submit").addClass("hide-loading");
-//       // For failed icon just replace ".done" with ".failed"
-//       $(".done").addClass("finish");
-//     }, 3000);
-//     setTimeout(function() {
-//       $(".submit").removeClass("loading");
-//       $(".submit").removeClass("hide-loading");
-//       $(".done").removeClass("finish");
-//       $(".failed").removeClass("finish");
-//     }, 5000);
-//   })
-// });
 
-// FORM SUBMISSION LOGIC
-
-// $(function() {
-//   $('#body').addClass('loaded');
-// });
-
-// $(document).ready(function() {
-
-//   setTimeout(function(){
-//       $('body').addClass('loaded');
-//   }, 3000);
-
-// });
-// window.onload= function() {
-//   document.getElementById("loader").style.display = "none"
-// }
-
-// var myVar;
-
-// function myLoader() {
-//   myVar = setTimeout(showPage, 1000);
-// }
-
-// function showPage() {
-//   document.getElementsByClassName("loader").style.display = "none";
-//   document.getElementsByClassName("hero-image").style.display = "flex";
-// }
-
-// // Get the modal
-// var modal = document.getElementById('myModal');
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-
-// // jQuery Button Animation
-// $(function() {
-//   $( "#button" ).click(function() {
-//     $( "#button" ).addClass( "onclic", 250, validate);
-//   });
-
-//   function validate() {
-//     setTimeout(function() {
-//       $( "#button" ).removeClass( "onclic" );
-//       $( "#button" ).addClass( "validate", 450, callback );
-//     }, 2250 );
-//   }
-//     function callback() {
-//       setTimeout(function() {
-//         $( "#button" ).removeClass( "validate" );
-//       }, 1250 );
-//     }
-//   });
